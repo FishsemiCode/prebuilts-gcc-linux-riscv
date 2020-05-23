8b57d6ca920c2075fbb53b2e24d971c37479cb20
niuyongqi <niuyongqi@fishsemi.com>
2020-05-23 10:13:47 +0800

    Initial checkin multilib riscv-gnu-toolchain
    version: 8.2.0
    from: https://github.com/riscv/riscv-gnu-toolchain
    commit: 411d13
    build with: --enable-multilib
    
    Signed-off-by: niuyongqi <niuyongqi@fishsemi.com>

---
+++ lib/gcc/riscv64-unknown-elf/8.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/yanjiong/Project/os/RISC-V/riscv/riscv-gnu-toolchain/riscv-gcc/configure --target=riscv64-unknown-elf --prefix=/home/yanjiong/riscv --disable-shared --disable-threads --enable-languages=c,c++ --with-system-zlib --enable-tls --with-newlib --with-sysroot=/home/yanjiong/riscv/riscv64-unknown-elf --with-native-system-header-dir=/include --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --src=.././riscv-gcc --enable-checking=yes --enable-multilib --with-abi=lp64d --with-arch=rv64imafdc 'CFLAGS_FOR_TARGET=-Os  -mcmodel=medlow' 'CXXFLAGS_FOR_TARGET=-Os  -mcmodel=medlow'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64d" }, { "arch", "rv64imafdc" } };
